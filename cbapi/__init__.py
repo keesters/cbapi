68a3312c025d4e229978f795db3af2caa179ed87
keesters <68505174+keesters@users.noreply.github.com>
2020-08-23 23:42:21 -0400

    Update __init__.py

--- cbapi/__init__.py
+++ cbapi/__init__.py
@@ -1,8 +1,11 @@
-__all__ = ['cbapi']
+__version__ = '1.0.0'
+__author__ = 'Adam Kessler'
 
-from .cbapi import set_api_key
-from .cbapi import get_api_key
-from .cbapi import trigger_api
-from .cbapi import get_data
-from .cbapi import companies
-from .cbapi import people
+# from .cbapi import set_api_key
+# from .cbapi import get_api_key
+# from .cbapi import trigger_api
+# from .cbapi import get_data
+# from .cbapi import companies
+# from .cbapi import people
+
+# __all__ = ['cbapi']
