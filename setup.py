de61df10b20572e64bb560bbccdf0b1f7e663929
keesters <68505174+keesters@users.noreply.github.com>
2020-08-24 00:00:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='cbapi',
     packages=['cbapi'],
-    url='https://github.com/keesters/baruch-mfe-python-summer/tree/master/cbapi',
+    url='https://github.com/keesters/cbapi.git',
     description='CrunchBase API for pulling people and organization data',
     long_description=open('README.md').read(),
     install_requires=['requests','pandas'],
-    # dependency_links = ['https://github.com/keesters/baruch-mfe-python-summer/tree/master/cbapi'],
     include_package_data=True,
 )
